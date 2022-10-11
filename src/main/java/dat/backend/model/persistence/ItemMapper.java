3af47e54ea4ed73fa62bf384ac2aebcbd0bd477d
Chriswihu <chris.wihu@gmail.com>
2022-10-11 15:40:04 +0200

    Ready for Wednesday

---
+++ src/main/java/dat/backend/model/persistence/ItemMapper.java
@@ -0,0 +1,44 @@
+package dat.backend.model.persistence;
+
+import dat.backend.model.entities.Item;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+class ItemMapper
+{
+    static List<Item> getItems(ConnectionPool connectionPool)
+    {
+        List<Item> itemList = new ArrayList<>();
+
+        String sql = "SELECT * FROM Item";
+
+        try(Connection connection = connectionPool.getConnection())
+        {
+            try(PreparedStatement ps = connection.prepareStatement(sql))
+            {
+                ResultSet rs = ps.executeQuery();
+                while (rs.next())
+                {
+                    int id = rs.getInt("item_id");
+                    String name = rs.getString("name");
+                    boolean done = rs.getBoolean("done");
+                    Timestamp created = rs.getTimestamp("created");
+                    String username = rs.getString("username");
+
+                    Item newItem = new Item(id, name, done, created, username);
+
+                    itemList.add(newItem);
+                }
+            }
+            catch (SQLException e)
+            {
+                e.printStackTrace();
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return itemList;
+    }
+}
